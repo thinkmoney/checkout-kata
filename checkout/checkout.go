9a07ed7671d1d49c9a6466874790c1ff9bac2764
wsl-11-2023 <lewfar99@gmail.com>
2024-02-12 13:59:38 +0000

    fix(checkout): Account for multiple deal applications

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -2,8 +2,6 @@ package checkout
 
 import (
 	"errors"
-	"fmt"
-	"math"
 )
 
 type ScannedItem struct {
@@ -47,9 +45,6 @@ func (s Scanner) GetTotalPrice() int {
     total := 0 
 
     for key, scannedItem := range s.scannedItems {
-        // NOTE: There's a potentially missed defensive coding opportunity here.
-        // Theoretically, the item might not exist, though practically the only way of entering the data is 
-        // via the Scan function, which already handles the case where an item is not registered
         item := allItems[key]
 
         if item.SpecialPrice.Quantity > scannedItem.quantityScanned {
@@ -63,7 +58,13 @@ func (s Scanner) GetTotalPrice() int {
         }
         
         if item.SpecialPrice.Quantity < scannedItem.quantityScanned {
-        }
+            // Calculate the total price for items up to the special price quantity
+            total += (scannedItem.quantityScanned / item.SpecialPrice.Quantity) * item.SpecialPrice.Price
+
+            // Calculate the total price for the remaining items at the regular price
+            remainingQuantity := scannedItem.quantityScanned % item.SpecialPrice.Quantity
+            total += remainingQuantity * item.UnitPrice
+        }   
     }
 
     return total
