82d1d118554b3b10eb6ffa7ee4c33267d42d828c
wsl-11-2023 <lewfar99@gmail.com>
2024-02-12 12:41:27 +0000

    feat(checkout): Add return values to Scan function
    Scan now returns a `ScannedItem` and `error` pair. A `New` convenience
    method is added for ease of consumption of the module.

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -3,24 +3,38 @@ package checkout
 import "errors"
 
 type ScannedItem struct {
-	quantityScanned int
+	quantityScanned uint
 }
 
 type Scanner struct {
 	scannedItems map[rune]ScannedItem
 }
 
-func (s *Scanner) Scan(SKU rune) error {
-	if _, itemExists := allItems[SKU]; !itemExists {
-		return errors.New("Item SKU does not exist in system")
-	}
+func New() Scanner {
+    return Scanner{
+        scannedItems: make(map[rune]ScannedItem),
+    }
+}
 
+var (
+    ErrItemDoesntExist = errors.New("Item SKU does not exist in system")
+    emptyScannedItem = ScannedItem{}
+)
+
+func (s *Scanner) Scan(SKU rune) (ScannedItem, error) {
+	if _, itemExists := allItems[SKU]; !itemExists {
+		return emptyScannedItem, ErrItemDoesntExist
+    }
+        
 	if scannedItem, scannedItemExists := s.scannedItems[SKU]; scannedItemExists {
 		scannedItem.quantityScanned++
 		s.scannedItems[SKU] = scannedItem
+        return scannedItem, nil
 	} else {
-		s.scannedItems[SKU] = ScannedItem{}
+        toReturn := ScannedItem{
+            quantityScanned: 1,
+        }
+		s.scannedItems[SKU] = toReturn
+        return toReturn, nil
 	}
-
-	return nil
 }
