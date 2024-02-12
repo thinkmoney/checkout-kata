a764d552a0f4f40958fbf66629593ed23f4f9ee4
wsl-11-2023 <lewfar99@gmail.com>
2024-02-12 12:17:33 +0000

    style(gofmt): Go fmt
    My current vim setup doesn't have go auto fmt script setup (new machine)
    so will just run this periodically for the time being.

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -3,24 +3,24 @@ package checkout
 import "errors"
 
 type ScannedItem struct {
-    quantityScanned int
+	quantityScanned int
 }
 
 type Scanner struct {
-    scannedItems map[rune]ScannedItem
+	scannedItems map[rune]ScannedItem
 }
 
 func (s *Scanner) Scan(SKU rune) error {
-    if _, itemExists := allItems[SKU]; !itemExists {
-        return errors.New("Item SKU does not exist in system")
-    }
+	if _, itemExists := allItems[SKU]; !itemExists {
+		return errors.New("Item SKU does not exist in system")
+	}
 
-    if scannedItem, scannedItemExists := s.scannedItems[SKU]; scannedItemExists { 
-        scannedItem.quantityScanned++
-        s.scannedItems[SKU] = scannedItem
-    } else {
-        s.scannedItems[SKU] = ScannedItem{}
-    }
+	if scannedItem, scannedItemExists := s.scannedItems[SKU]; scannedItemExists {
+		scannedItem.quantityScanned++
+		s.scannedItems[SKU] = scannedItem
+	} else {
+		s.scannedItems[SKU] = ScannedItem{}
+	}
 
-    return nil
+	return nil
 }
--- checkout/itemList.go
+++ checkout/itemList.go
@@ -1,39 +1,39 @@
 package checkout
 
 type SpecialPrice struct {
-    Quantity uint
-    Price int
+	Quantity uint
+	Price    int
 }
 
 type Item struct {
-    UnitPrice int
-    SpecialPrice SpecialPrice
+	UnitPrice    int
+	SpecialPrice SpecialPrice
 }
 
 var allItems map[rune]Item = make(map[rune]Item)
 
 func init() {
-    allItems['A'] = Item{
-        UnitPrice: 50,
-        SpecialPrice: SpecialPrice{
-            Quantity: 3,
-            Price: 130,
-        },
-    }
+	allItems['A'] = Item{
+		UnitPrice: 50,
+		SpecialPrice: SpecialPrice{
+			Quantity: 3,
+			Price:    130,
+		},
+	}
 
-    allItems['B'] =  Item{
-        UnitPrice: 30,
-        SpecialPrice: SpecialPrice{
-            Quantity: 2,
-            Price: 45,
-        },
-    }
+	allItems['B'] = Item{
+		UnitPrice: 30,
+		SpecialPrice: SpecialPrice{
+			Quantity: 2,
+			Price:    45,
+		},
+	}
 
-    allItems['C'] =  Item{
-        UnitPrice: 20,
-    }
+	allItems['C'] = Item{
+		UnitPrice: 20,
+	}
 
-    allItems['D'] = Item{
-        UnitPrice: 15,
-    }
+	allItems['D'] = Item{
+		UnitPrice: 15,
+	}
 }
